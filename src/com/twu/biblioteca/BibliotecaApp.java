c235deca6c544238ca800573392cbe64d26b09d7
Christoph Wong <wong.christoph@gmail.com>
2015-01-07 12:47:09 +0800

    when app starts, welcome message displays

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp application = new BibliotecaApp();
+        application.welcome();
+    }
+
+    public void welcome(){
+        System.out.println("Welcome to the Biblioteca Application");
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,31 @@
+package com.twu.biblioteca;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest {
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    @Before
+    public void setupStreams(){
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void cleanUpStreams(){
+        System.setOut(null);
+    }
+
+    private BibliotecaApp application = new BibliotecaApp();
+
+    @Test
+    public void testDisplayOfWelcomeMessage() {
+        application.welcome();
+        assertEquals("Welcome to the Biblioteca Application\n",outputStream.toString());
+    }
+}
